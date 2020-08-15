36e12428627744e5823f33c8d0ca481de84a4331
Marcelo-Feliz <mf.marcelofeliz@gmail.com>
2020-08-15 12:28:59 +0100

    +++

---
+++ Web/serving_files/exercicios/5/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+}
+func main() {
+	http.HandleFunc("/", dog)
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("./public"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+func dog(w http.ResponseWriter, req *http.Request) {
+	err := tpl.Execute(w, nil)
+	if err != nil {
+		log.Fatalln("template didn't execute: ", err)
+	}
+}
